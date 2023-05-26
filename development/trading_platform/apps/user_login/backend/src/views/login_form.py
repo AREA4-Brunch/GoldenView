2b769384a9872cd40295917ff08ea11a9165c8ce
Aleksandar Radenkovic <ra200272d@student.etf.bg.ac.rs>
2023-05-26 23:13:45 +0200

    Fixed and setup user_login app for the beginning of database integration.
    Change-Id: I1511956c473ba803791d2cb531f56db5ddd164bc

--- development/trading_platform/apps/user_login/backend/src/views/login_form.py
+++ development/trading_platform/apps/user_login/backend/src/views/login_form.py
@@ -1,12 +1,46 @@
 from django.http import HttpRequest
 
+from apps.user_management.backend.src.utils.user_login import login_user
+from apps.common.backend.utils.error_handling import internal_error_catcher
 
-def validate_login_form(request: HttpRequest):
-    is_valid = False
 
-    request.session['usr_err'] = 'Err msg user'
-    request.session['pwd_err'] = 'Err msg pwd'
+def get_cleaned_data(request: HttpRequest):
+    user_data = {}
 
-    request.session['usr'] = 'Entered username'
+    # store only the non confidential answers
+    def store_previous_answers():
+        request.session['usr'] = request.POST.get('usr')
 
-    return is_valid
+    def parse_data():
+        is_valid = True
+
+        try:  # process username
+            user_data["usr"] = request.POST["username"]
+        except Exception as e:
+            is_valid = False
+            request.session['usr_err'] = 'Invalid username'
+
+        try:  # process password
+            user_data["pwd"] = request.POST["password"]
+        except Exception as e:
+            is_valid = False
+            request.session['pwd_err'] = 'Invalid password'
+
+        if not is_valid:
+            store_previous_answers()
+            raise Exception('Form is not valid')
+
+    def main():
+        parse_data()
+
+    internal_error_catcher(request, main)
+
+    return user_data
+
+
+def login(user_data: dict):
+    username = user_data['usr']
+    password = user_data['pwd']
+
+    login_user(username=username, password=password)
+    return
--- development/trading_platform/apps/user_login/views.py
+++ development/trading_platform/apps/user_login/views.py
@@ -1,16 +1,22 @@
+import logging
+
 from django.shortcuts import render, redirect
 from django.http import HttpRequest
 
-from .backend.src.views.login_form import validate_login_form
-from apps.common.backend.utils.login_user import login_user
+from .backend.src.views import login_form as login_form_backend
+
 
 # Create your views here.
 
 
 def login(request: HttpRequest):
+    # fetch the data from the post
+    # if invalid data redirect the
     context = {
         'usr_err': request.session.pop('usr_err', None),
         'pwd_err': request.session.pop('pwd_err', None),
+
+        'internal_err': request.session.pop('gender_err', None),
 
         'usr': request.session.pop('usr', None),
     }
@@ -27,11 +33,25 @@ def login_form(request: HttpRequest):
         request.session['link_404'] = request.get_full_path()
         return redirect('page_404')
 
-    # TODO: validate and login
-    if not validate_login_form(request):
+    try:
+        # validate and parse the data from the form,
+        # throws if form is invalid
+        user_data = login_form_backend.get_cleaned_data(request)
+
+        # login the user
+        try:
+            login_form_backend.login(user_data)
+
+        except Exception as e:
+            request.session['internal_err'] = str(e)
+            logging.error(f'Internal error: {e}')
+            raise e
+
+    except Exception as e:
+        # error message have already been set
         return redirect('login')
 
-    # TODO: login the user before home redirect
-    login_user()
+    # check if the user has accepted the terms of agreement, else
+    # perhaps redirect to disclaimer page
 
     return redirect('home')
