b6a45d9375e09bee552089e314c64238030111a1
Aleksandar Radenkovic <ra200272d@student.etf.bg.ac.rs>
2023-06-19 12:56:50 +0200

    Added css dynamically to preferred company in compare companies.
    Change-Id: Ibe2cc0c8188960563a0eebd0d136ea2661075d8f

--- development/trading_platform/apps/comparingCompanies/views.py
+++ development/trading_platform/apps/comparingCompanies/views.py
@@ -2,11 +2,11 @@ from django.http import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def comparingCompanies(request: HttpRequest):
 
     return render(
         request,
-        "comparingCompanies/footer/comparingCompanies.html",
+        "comparingCompanies/comparing_companies_page/comparingCompanies.html",
     )
-
-
